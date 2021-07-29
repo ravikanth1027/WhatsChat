8346f6960943c9fa7a3da9293f28e87f758e7cb8
ravi786 <ravikanth1027@gmail.com>
2021-07-29 17:31:06 +0530

    Fixed ff issue

--- voipOffice_Chat/src/components/ManageApp.js
+++ voipOffice_Chat/src/components/ManageApp.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import Axios from 'axios'
 import './ManageApp.css';
 import * as pageBase from '../pageBase'
+
+async function addcontact(state) {
+    console.log("addcontact:", state)
+    var payload = {
+        "name" : state.name,
+    "number" : state.number,
+    "avatar" : state.avatar,
+    "phonenumber" : state.phonenumber   // Please adjust this - THis is the logged in persons
+    }
+    var url = 'http://localhost:8080/addcontact'
+    //var url = 'http://108.60.134.228:8080/deleteContact'
+ return fetch(url, {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json',
+     Accept : 'application/json'
+   },
+   body: JSON.stringify(payload)
+ })
+   .then(data => data.json())
+}
+
 class ManageApp extends Component{
 constructor(props) {
     super(props);
@@ -16,13 +38,17 @@ constructor(props) {
   }
 
   handleSubmit = (event) => {
-    //var url = 'http://localhost:8080/'
-    var url = 'http://108.60.134.228:8080/'
+    console.log("handleSubmit121******************", this.state)
+    this.state.phonenumber = this.props.dataFromParent
+    /*var url = 'http://localhost:8080/'
+    //var url = 'http://108.60.134.228:8080/'
     
     this.state.phonenumber = this.props.dataFromParent
-    const response= Axios.post(url+'addcontact', this.state);
+    const response= Axios.post(url+'addcontact', this.state);*/
+    const response = addcontact(this.state)
     return response.json();
     event.preventDefault();
+    
 }
 
 showForm = () => {
